f992274988ad57017686c211bdc0311759d85366
zaiaaa <gustavo.0801214@gmail.com>
2023-10-07 22:42:37 -0300

    inclusao do header separado, para inclusao

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<div class="navbar">
+        <div class="icon-search">
+            <h2><a href="<?php echo BASEURL; ?>">TicketWave</a></h2>
+            
+            <div class="search">
+                <form action="#">
+                    <input type="text" class="corner-mask">
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </div>
+        </div>
+
+        <div class="nav-buttons">
+            <nav>
+                <ul>
+                    <li class="item"><a class="active" href="">Home</a></li>
+                    <li class="item"><a href="">Eventos próximos</a></li>
+                    <li><a href="">Sobre</a></li>
+                </ul>
+            </nav>
+            <div class="buttons">
+                <button class="cad">Cadastrar</button>
+                <button class="ent">Entrar</button>
+            </div>
+        </div>
+    </div>
